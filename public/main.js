b18aa9e5ce0203d306313c1ba74968a4cedaa3cb
Rennan Prysthon <rennandelcastillo@gmail.com>
2021-05-26 20:20:33 -0300

    [feat] added websocket

--- public/main.js
+++ public/main.js
@@ -1,7 +1,9 @@
+const socket = io('/')
+
 var table = document.querySelector('#table');
 
-const black = `<i id="black" class="fas fa-chess-knight"></i>`;
-const white = `<i id="white" class="fas fa-chess-knight"></i>`;
+const BLACK = `<i id="black" class="fas fa-chess-knight"></i>`;
+const WHITE = `<i id="white" class="fas fa-chess-knight"></i>`;
 
 for(var i = 1; i <=8; i++) {
   for(var j = 1; j <=8; j++) {
@@ -10,21 +12,11 @@ for(var i = 1; i <=8; i++) {
   }
 }
 
-var n1 = Math.floor(Math.random() * 8) + 1;
-var n2 = Math.floor(Math.random() * 8) + 1;
-
-var pos1 = document.querySelector(`#t1${n1}`);
-var pos2 = document.querySelector(`#t8${n2}`);
-
-pos1.innerHTML += black;
-pos2.innerHTML += white;  
-
 var rounds = 0;
 var avaliablePositions = [];
 var usedPositions = [];
 var focused;
 var playerWay = 'white';
-
 
 document.querySelector('main').innerHTML += '<div class="back" onclick="dismiss(false)"></div>';
 document.querySelector('body').innerHTML += `
@@ -35,10 +27,89 @@ document.querySelector('body').innerHTML += `
   </div>
 `;
 
-function initGame() {
-  var table = document.querySelector('#table');
-  document.querySelector('.back').remove();
-  document.querySelector('.modal').remove();
+socket.on('game-full', () => {
+  document.querySelector('main').innerHTML = '<div class="back" onclick="dismiss(false)"></div>';
+  document.querySelector('body').innerHTML = `
+  <div class="modal">
+    Jogo já esta cheio
+  </div>
+`;
+})
+
+socket.on('put-white', position => {
+  let pos = document.querySelector(`#t8${position}`);
+  pos.innerHTML += WHITE;
+})
+
+socket.on('put-black', position => {
+  let pos = document.querySelector(`#t1${position}`);
+  pos.innerHTML += BLACK;
+})
+
+socket.on('update-positions', (pUsedPositions) => {
+  usedPositions = pUsedPositions
+})
+
+socket.on('show-winner', value => {
+  var name = value === 'white' ? 'black' : 'white';
+  showModal(name + ' wins!')
+  socket.emit('reset-server-values')
+})
+
+socket.on('update-all', (positions) => {
+  positions.map(p => {
+    let element = document.querySelector(`#${p}`);
+    usedPositions.push(element)
+    element.classList.add('used')
+  })
+}) 
+
+socket.on('reset-all-values', () => {
+
+  usedPositions.map(p => {
+    let element = document.querySelector(`#${p.id}`);
+    element.classList.remove('used')
+  })
+
+  document.querySelector('#black').remove()
+  document.querySelector('#white').remove()
+
+  rounds = 0;
+  avaliablePositions = [];
+  usedPositions = [];
+  focused = null
+  playerWay = 'white';
+  
+  table.innerHTML = ''
+
+  for(var i = 1; i <=8; i++) {
+    for(var j = 1; j <=8; j++) {
+      var el = `<span id="t${i}${j}" class="house" onclick="select(this)"></span>`
+      table.innerHTML += el;
+    }
+  }
+})
+
+socket.on('update-horse', (pos1, pos2, pPlayerWay, pRounds) => {
+  let deOndeVeio = document.querySelector(`#${pos1}`)
+  let paraOndeVai = document.querySelector(`#${pos2}`)
+
+  let horse = deOndeVeio.firstElementChild;
+  paraOndeVai.appendChild(horse)
+
+  deOndeVeio.innerHTML = ''
+  deOndeVeio.classList.remove('focused')
+  playerWay = pPlayerWay;
+
+  uncheckAll()
+
+  rounds = pRounds;
+
+  const name = playerWay === 'black' ? 'Preto' : 'Branco';
+  document.querySelector('#player').innerHTML = name
+})
+
+socket.on('start-game', () => {
   avaliableAll();
 
   rounds = 0;
@@ -47,23 +118,12 @@ function initGame() {
   focused = null;
   playerWay = 'white';
 
-  table.innerHTML = '';   
+  document.querySelector('.back').remove();
+  document.querySelector('.modal').remove();
+})
 
-  for(var i = 1; i <=8; i++) {
-    for(var j = 1; j <=8; j++) {
-      var el = `<span id="t${i}${j}" class="house" onclick="select(this)"></span>`
-      table.innerHTML += el;
-    }
-  }
-
-  var n1 = Math.floor(Math.random() * 8) + 1;
-  var n2 = Math.floor(Math.random() * 8) + 1;
-  
-  var pos1 = document.querySelector(`#t1${n1}`);
-  var pos2 = document.querySelector(`#t8${n2}`);
-  
-  pos1.innerHTML += black;
-  pos2.innerHTML += white;  
+function initGame() {
+  socket.emit('enter-player', '')
 }
 
 function select(e) {    
@@ -74,15 +134,10 @@ function select(e) {
   }
   if (focused) {
     if (avaliablePositions.includes(e)) {
-      var horse = focused.firstElementChild;
-      focused.innerHTML = ''
-      e.appendChild(horse)
-      uncheckAll()
-      markUsed(focused);
-      focused.classList.remove('focused')
+      socket.emit('change-horse', focused.id, e.id, rounds)
+      socket.emit('mark-position', focused.id)
       focused = null;
-      rounds++;
-      verifyNextPlayer()
+      return;
     }
   } else {
     if (e.firstElementChild) {    
@@ -94,21 +149,11 @@ function select(e) {
       checkAvaliablePlaces(getValueFromPosition(e))
       if(avaliablePositions.length < 1) {
         if(e.firstElementChild.id) {
-          endGame(e.firstElementChild.id)
+          socket.emit('end-game', e.firstElementChild.id)
         }
       }
     }
   }
-}
-
-function verifyNextPlayer () {
-  if(rounds % 2 != 0) {
-    playerWay = 'black'
-  } else {
-    playerWay = 'white'
-  }
-  const name = playerWay === 'black' ? 'Preto' : 'Branco';
-  document.querySelector('#player').innerHTML = name
 }
 
 const getValueFromPosition = (e) => ({p1: e.id.substring(1, 2), p2: e.id.substring(2, 3)});
@@ -154,14 +199,9 @@ function avaliableAll() {
 }
 
 function markUsed(e) {
-  usedPositions.push(e)
-  e.classList.add('used')
+  socket.emit('mark-position', e.id)
 }
 
-function endGame(e) {
-  var name = e === 'white' ? 'BLACK' : 'WHITE';
-  showModal(name + ' wins!')
-}
 
 function showModal(e) {
   var name = `<h3>${e}</h3>`
--- server.js
+++ server.js
@@ -1,12 +1,68 @@
 const express = require('express');
 const app = express()
 const server = require('http').Server(app);
+const io = require('socket.io')(server)
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+var players = [];
+var usedPositions = [];
+var rounds = 0;
+
+io.on('connection', socket => {
+  if(players.length == 2) {
+    socket.emit('game-full')
+  }
+
+  socket.on('enter-player', socket => {
+   
+    if(players.length == 1){
+      players.push('white')
+      io.emit('put-white', Math.floor(Math.random() * 8) + 1)
+    }
+    if (players.length == 0) {
+      players.push('black')
+      io.emit('put-black', Math.floor(Math.random() * 8) + 1)
+    } 
+  
+    if(players.length == 2) {
+      io.emit('start-game')
+    }
+  })
+
+  socket.on('mark-position', (position) => {
+    usedPositions.push(position);
+
+    io.emit('update-all', usedPositions)
+  })
+
+  socket.on('change-horse', (pos1, pos2) => {
+    rounds++;
+    let playerWay = 'white'
+    if(rounds % 2 != 0) {
+      playerWay = 'black'
+    }
+
+    io.emit('update-horse', pos1, pos2, playerWay, rounds)
+  })
+
+
+  socket.on('end-game', (value)=> {
+    io.emit('show-winner', value)
+  })
+
+  socket.on('reset-server-values', () => {
+    rounds = 0
+    usedPositions = []
+    playerWay = 'white'
+    players = []
+    io.emit('reset-all-values', '')
+  })
 })
 
 server.listen(3000)
